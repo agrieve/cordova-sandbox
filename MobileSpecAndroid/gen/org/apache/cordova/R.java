24f7e5b673c2c5d43d71b60b37cf7e84a083ba04
Andrew Grieve <agrieve@chromium.org>
2012-09-18 10:26:49 -0400

    update mobile-spec

--- MobileSpecAndroid/gen/org/apache/cordova/R.java
+++ MobileSpecAndroid/gen/org/apache/cordova/R.java
@@ -24,7 +24,5 @@ public final class R {
     }
     public static final class xml {
         public static final int config=0x7f040000;
-        public static final int cordova=0x7f040001;
-        public static final int plugins=0x7f040002;
     }
 }
